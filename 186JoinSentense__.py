ed90eb4e4d4bff45b2456c54c13250c79536e815
Alogenesis <nononosolo@hotmail.com>
2021-10-17 11:00:25 +0700

    186 Complete

--- 186JoinSentense__.py
+++ 186JoinSentense__.py
@@ -1,29 +1,12 @@
-'''
-ให้ทำการเรียบเรียงประโยคที่กำหนดให้ออกมาเป็นประโยคเดียว
-บรรทัดแรกจำนวนประโยค
-บรรทัดที่เหลือ เป็นประโยคที่ต้องนำมารวมกัน
+user_input = int(input())
+lst = []
 
-
-'''
-
-x = 7 #int(input())
-lst = [
-'afraid of greatness.',
-'Be not afraid',
-'some achieve greatness,',
-'greatness thrust upon them.',
-'greatness. Some',
-'Some are born great, some',
-'greatness, and others have greatness',
-]
-
-'''
-for i in range(0,x):
+for i in range(0,user_input):
     y = input()
     lst.append(y)
-'''
 
-print(lst)
+# input management
+#print(lst)
 first_lst = []
 rearrange_lst = []
 dot = []
@@ -37,53 +20,127 @@ for i in lst:
     elif i[-1] == '.' :
         dot.append(i)
 
-print(first_lst)
-print(rearrange_lst)
-print(dot)
+#print('first_lst =', first_lst)
+#print('rearrange_lst =',rearrange_lst)
+#print('dot =', dot)
 
-first1_spl = first_lst[0].split(' ')
-print(first1_spl)
+# add list member to main list
+first_main_lst = []
+for i in first_lst :
+    x = i.split(' ')
+    first_main_lst.append(x)
 
-first2_spl = first_lst[1].split(' ')
-print(first2_spl)
-
-# Run หาประโยคแรก
-
-print(first_lst[0])
-
-# Algo run หาประโยคกลางมาต่อ
-print('---------------------')
-current = first_lst[0].split(' ')
-print(current)
-# Run หาประโยคต่อ
-for i in range(0,len(rearrange_lst)):
-    mid_spl = rearrange_lst[i].split(' ')
-    print('ประโยคปัจจุบัน',mid_spl)
-    if mid_spl[1] == current[-2] and mid_spl[0] == current[-1]:
-        print('Same 2')
-        print(rearrange_lst[i])
-    elif mid_spl[0] == current[-1]:
-        print('Same 1')
-        print(rearrange_lst[i])
-    else :
-        print('No Same')
-
-# Run หาประโยคท้าย
-for i in range(0,len(dot)):
-    dot_spl = dot[i].split(' ')
-    print('ประโยคท้ายปัจจุบัน',dot_spl)
-    if dot_spl[1] == current[-2] and dot_spl[0] == current[-1]:
-        print('Same 2')
-        print(dot[i])
-    elif dot_spl[0] == current[-1]:
-        print('Same 1')
-        print(dot[i])
-    else:
-        print('No Same')
+mid_main_pop = []
+mid_main_lst = [] #Add ทั้งหมดลง mid
+for i in first_lst : #Add first ไปใน mid
+    x = i.split(' ')
+    mid_main_lst.append(x)
+    mid_main_pop.append(x)
 
 
+for i in rearrange_lst :    #Add mid ลง mid
+    x = i.split(' ')
+    mid_main_lst.append(x)
+    mid_main_pop.append(x)
+
+#last add to mid
+for i in dot :
+    x = i.split(' ')
+    mid_main_lst.append(x)
+    mid_main_pop.append(x)
+
+#print(first_main_lst)
+#print(mid_main_lst)
+
+
+#Algorhytm
+countlst = user_input
+mainlst = []
+sublst =  []
+
+
+# Add main list
+mainlst.append(mid_main_lst[0]) # add ประโยคแรกลงไป
+countlst -= 1
+sublst.append(mid_main_lst[0]) # sub list ไว้เช็ค
+current_checker = mid_main_lst[0] #'ไว้สำหรับเช็ค'
+mid_main_lst.pop(0)
+#print('current_checker =',current_checker)
+#print('เหลือ', mid_main_lst)
+#print('เหลือกี่ประโยคที่ต้องใส่ =',countlst)
+#mid_main_lst.append(x)
+#mid_main_pop.append(x)
+
+#print('=== start Loop ===')
+
+run = len(mid_main_lst)
+while run > 0:
+
+    unmatch = len(mid_main_lst)
+    for i in mid_main_lst:
+
+        #print('checker = ', current_checker, 'i=',i , )
+        #print('เช็ค 1 คำ', current_checker[-1], i[0])
+        #print('เช็ค 2 คำ', current_checker[-2], i[1])
+        if current_checker[-2] == i[0] and current_checker[-1] == i[1]:  # ท้าย2 เท่ากับต้น 2
+            sublst.append(i)  # ลง sub ไว้เช็ค
+            x = i  # ลบตัวหน้า 2 ตัว
+            y = x[2:]
+            mainlst.append(y)  # mid main ที่ i
+            countlst -= 1
+            current_checker = i
+            #print('เหมือน 2')
+            #print('main lst = ', mainlst)
+            #print('sub =', sublst)
+            mid_main_lst.remove(i)
+            #print('เหลือ', mid_main_lst)
+
+            break
+        elif current_checker[-1] == i[0] :  # ท้าย1 เท่ากับต้น 1
+            sublst.append(i)  # ลง sub ไว้เช็ค
+            x = i  # ลบตัวหน้า 2 ตัว
+            y = x[1:]
+            mainlst.append(y)  # mid main ที่ i
+            countlst -= 1
+            current_checker = i
+            #print('เหมือน 1')
+            #print('main lst = ', mainlst)
+            #print('sub =', sublst)
+            mid_main_pop.remove(i)
+            mid_main_lst.remove(i)
+            #print('เหลือ', mid_main_lst)
+            break
+        elif current_checker[-3] == i[0]:
+            sublst.append(i)  # ลง sub ไว้เช็ค
+            x = i  # ลบตัวหน้า 2 ตัว
+            y = x[3:]
+            mainlst.append(y)  # mid main ที่ i
+            countlst -= 1
+            current_checker = i
+            #print('เหมือน 2')
+            #print('main lst = ', mainlst)
+            #print('sub =', sublst)
+            mid_main_lst.remove(i)
+            #print('เหลือ', mid_main_lst)
+            
+        elif unmatch == 0 :
+            break
+        else:
+            unmatch -= 1
+            #print('ไม่ตรง-1 เหลือ', unmatch)
+
+    run -= 1
 
 
 
 
 
+
+
+#answer
+#print('answer')
+#print(mainlst)
+#print(sublst)
+#print(countlst)
+for i in mainlst :
+    print(*i,end=' ')
--- 999TestCode.py
+++ 999TestCode.py
@@ -1,39 +1,164 @@
-user_input = input()
-spl1, spl2 = user_input.split(' ')
-last = int(spl1)
-loop_get = int(spl2)
+user_input = 4 #int(input())
+lst = [
+'the day, thou canst not then be false to any man.',
+'This above all: to thine own self',
+'must follow, as the night the day, thou',
+'own self be true, and it must follow, as',
+]
 
-way = []
-for i in range(0,loop_get) :
-    x = input()
-    way.append(x)
-#print(way)
+'''
+for i in range(0,x):
+    y = input()
+    lst.append(y)
+'''
+# input management
+print(lst)
+first_lst = []
+rearrange_lst = []
+dot = []
+answer_lib = []
 
-time = 0
-#find last
-target = last
-way_lst = []
-while target != 1 :
-    for i in way:
-        way_temp = []
-        y = i.split(' ')
-        way_lst.append(y)   #add for print
-        way_temp.append(y)  #add for split
-        a11,a22,a33 = i.split(' ')
-        a1 = int(a11)
-        a2 = int(a22)
-        a3 = int(a33)
-        if a2 == target :
-            #print('>>> target =',a2)
-            time += a3
-            target = a1
+for i in lst:
+    if i[0].isupper():
+        first_lst.append(i)
+    elif i[-1] != '.':
+        rearrange_lst.append(i)
+    elif i[-1] == '.' :
+        dot.append(i)
+
+print('first_lst =', first_lst)
+print('rearrange_lst =',rearrange_lst)
+print('dot =', dot)
+
+# add list member to main list
+first_main_lst = []
+for i in first_lst :
+    x = i.split(' ')
+    first_main_lst.append(x)
+
+mid_main_pop = []
+mid_main_lst = [] #Add ทั้งหมดลง mid
+for i in first_lst : #Add first ไปใน mid
+    x = i.split(' ')
+    mid_main_lst.append(x)
+    mid_main_pop.append(x)
+
+
+for i in rearrange_lst :    #Add mid ลง mid
+    x = i.split(' ')
+    mid_main_lst.append(x)
+    mid_main_pop.append(x)
+
+#last add to mid
+for i in dot :
+    x = i.split(' ')
+    mid_main_lst.append(x)
+    mid_main_pop.append(x)
+
+print(first_main_lst)
+print(mid_main_lst)
+
+
+#Algorhytm
+countlst = user_input
+mainlst = []
+sublst =  []
+
+
+# Add main list
+mainlst.append(mid_main_lst[0]) # add ประโยคแรกลงไป
+countlst -= 1
+sublst.append(mid_main_lst[0]) # sub list ไว้เช็ค
+current_checker = mid_main_lst[0] #'ไว้สำหรับเช็ค'
+mid_main_lst.pop(0)
+print('current_checker =',current_checker)
+print('เหลือ', mid_main_lst)
+print('เหลือกี่ประโยคที่ต้องใส่ =',countlst)
+#mid_main_lst.append(x)
+#mid_main_pop.append(x)
+
+print('=== start Loop ===')
+
+run = len(mid_main_lst)
+while run > 0:
+
+    unmatch = len(mid_main_lst)
+    for i in mid_main_lst:
+
+        print('checker = ', current_checker, 'i=',i , )
+        #print('เช็ค 1 คำ', current_checker[-1], i[0])
+        #print('เช็ค 2 คำ', current_checker[-2], i[1])
+
+        if current_checker[-2] == i[0] and current_checker[-1] == i[1]:  # ท้าย2 เท่ากับต้น 2
+            sublst.append(i)  # ลง sub ไว้เช็ค
+            x = i  # ลบตัวหน้า 2 ตัว
+            y = x[2:]
+            mainlst.append(y)  # mid main ที่ i
+            countlst -= 1
+            current_checker = i
+            print('เหมือน 2')
+            print('main lst = ', mainlst)
+            print('sub =', sublst)
+            mid_main_lst.remove(i)
+            print('เหลือ', mid_main_lst)
+
+            break
+        elif current_checker[-1] == i[0] :  # ท้าย1 เท่ากับต้น 1
+            sublst.append(i)  # ลง sub ไว้เช็ค
+            x = i  # ลบตัวหน้า 2 ตัว
+            y = x[1:]
+            mainlst.append(y)  # mid main ที่ i
+            countlst -= 1
+            current_checker = i
+            print('เหมือน 1')
+            print('main lst = ', mainlst)
+            print('sub =', sublst)
+            mid_main_pop.remove(i)
+            mid_main_lst.remove(i)
+            print('เหลือ', mid_main_lst)
+            break
+        elif current_checker[-3] == i[0]:
+            sublst.append(i)  # ลง sub ไว้เช็ค
+            x = i  # ลบตัวหน้า 2 ตัว
+            y = x[3:]
+            mainlst.append(y)  # mid main ที่ i
+            countlst -= 1
+            current_checker = i
+            print('เหมือน 2')
+            print('main lst = ', mainlst)
+            print('sub =', sublst)
+            mid_main_lst.remove(i)
+            print('เหลือ', mid_main_lst)
+
+        elif unmatch == 0 :
             break
         else:
-          pass
-            #print(way)('not target')
+            unmatch -= 1
+            print('ไม่ตรง-1 เหลือ', unmatch)
 
-        #print(way)('now target =',target)
-        #print(way)('time =',time)
+    run -= 1
 
-#print(way)('last target =',target)
-print(time)
+
+
+
+
+
+#answer
+print('answer')
+print(mainlst)
+print(sublst)
+print(countlst)
+for i in mainlst :
+    print(*i,end=' ')
+
+
+
+
+
+
+
+
+
+
+
+

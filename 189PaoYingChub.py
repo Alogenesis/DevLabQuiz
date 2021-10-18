02abe8ca6f4e60b14eae65a8869f2752063bc8ef
Alogenesis <nononosolo@hotmail.com>
2021-10-18 11:01:56 +0700

    189 Complete

---
+++ 189PaoYingChub.py
@@ -0,0 +1,12 @@
+'''
+Output เป่ายิ้งฉุบให้ชนะ
+Input : ROCK / PAPER / SCISSORS
+'''
+
+x = input()
+if x == 'ROCK' :
+    print('PAPER')
+elif x == 'PAPER' :
+    print('SCISSORS')
+else:
+    print('ROCK')
